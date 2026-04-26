f3d5142419a077d0c437b269dd2a6a2ad7e1a7a5
lxard <luxardd@gmail.com>
2026-04-26 12:04:41 +0700

    Add store info and product list to produkf.js
    Define store information and a list of products with details.

---
+++ produkf.js
@@ -0,0 +1,43 @@
+const STORE = {
+  nama: "FafnirGank",
+  whatsapp: "6285921967820"
+};
+
+const products = [
+  {
+    id: 1,
+    name: "iPhone XR 64GB",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
+    condition: "Second",
+    price: 2800000,
+    description: "Kondisi 85%, normal semua, minus pemakaian wajar.",
+    stock: 1
+  },
+  {
+    id: 2,
+    name: "Headset Gaming Rexus",
+    image: "https://images.unsplash.com/photo-1580894908361-967195033215",
+    condition: "Ori",
+    price: 150000,
+    description: "Barang baru, segel, suara jernih + mic.",
+    stock: 5
+  },
+  {
+    id: 3,
+    name: "Keyboard Mechanical",
+    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
+    condition: "Second",
+    price: 350000,
+    description: "Switch masih enak, RGB normal.",
+    stock: 1
+  },
+  {
+    id: 4,
+    name: "Mouse Logitech",
+    image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3",
+    condition: "Ori",
+    price: 120000,
+    description: "Original, garansi resmi.",
+    stock: 3
+  }
+];
