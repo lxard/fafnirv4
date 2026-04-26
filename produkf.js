7fdd6a8fab0c6cc37ddec0b23eed98cee788368d
lxard <luxardd@gmail.com>
2026-04-26 12:11:33 +0700

    Update product images and descriptions

--- produkf.js
+++ produkf.js
@@ -7,7 +7,7 @@ const products = [
   {
     id: 1,
     name: "iPhone XR 64GB",
-    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9",
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=400&q=60",
     condition: "Second",
     price: 2800000,
     description: "Kondisi 85%, normal semua, minus pemakaian wajar.",
@@ -15,17 +15,17 @@ const products = [
   },
   {
     id: 2,
-    name: "Headset Gaming Rexus",
-    image: "https://images.unsplash.com/photo-1580894908361-967195033215",
+    name: "Headset Gaming",
+    image: "https://images.unsplash.com/photo-1580894908361-967195033215?w=400&q=60",
     condition: "Ori",
     price: 150000,
-    description: "Barang baru, segel, suara jernih + mic.",
+    description: "Barang baru, suara jernih + mic.",
     stock: 5
   },
   {
     id: 3,
     name: "Keyboard Mechanical",
-    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8",
+    image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=400&q=60",
     condition: "Second",
     price: 350000,
     description: "Switch masih enak, RGB normal.",
@@ -34,7 +34,7 @@ const products = [
   {
     id: 4,
     name: "Mouse Logitech",
-    image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3",
+    image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=400&q=60",
     condition: "Ori",
     price: 120000,
     description: "Original, garansi resmi.",
